94acdc06b79cd9d6d90901e4343561ebefebe04d
sergi-s <52626795+sergi-s@users.noreply.github.com>
2021-06-05 21:40:58 +0200

    deactivate bot after winning a level

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -12,21 +12,35 @@ public class FinishLevel : MonoBehaviour
     public GameObject timeleft;
     public GameObject theScore;
     public GameObject totalScore;
-    public int timeCalc ;
-    public int scoreCalc ;
+    public int timeCalc;
+    public int scoreCalc;
     public int totalScored;
-	public GameObject LevelBlocker;
+    public GameObject LevelBlocker;
     public GameObject fadeOut;
+
+    void onStart()
+    {
+    }
     void OnTriggerEnter()
     {
+        // GameObject[] gos = GameObject.FindGameObjectsWithTag("Enime");
+        // foreach (GameObject go in gos)
+        // {
+        //     go.SetActive(false);
+        // }
+
+        GameObject varGameObject = GameObject.FindWithTag("Enime");
+        varGameObject.GetComponent<AiFollow>().enabled = false;
+
+
         GetComponent<BoxCollider>().enabled = false;
-		LevelBlocker.SetActive(true);
+        LevelBlocker.SetActive(true);
         LevelBlocker.transform.parent = null;
         timeCalc = GlobalTimer.extendScore * 100;
         timeleft.GetComponent<Text>().text = "Time Left: " + GlobalTimer.extendScore + "x 100";
         theScore.GetComponent<Text>().text = "Score: " + GlobalScore.currentScore;
         totalScored = GlobalScore.currentScore + timeCalc;
-        totalScore.GetComponent<Text>().text = "Total Score: "+ totalScored;
+        totalScore.GetComponent<Text>().text = "Total Score: " + totalScored;
         levelMusic.SetActive(false);
         levelTimer.SetActive(false);
         levelComplete.Play();
